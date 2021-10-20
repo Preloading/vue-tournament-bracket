8b8b363a296c4a5069246c2140d863c44ad293d0
Fernando IL <luffynando@gmail.com>
2021-10-20 17:09:39 -0500

    fix: added sass styles missed

--- src/Bracket.vue
+++ src/Bracket.vue
@@ -62,3 +62,92 @@ export default defineComponent({
   },
 });
 </script>
+
+<style lang="sass">
+.vtb-item
+  display: flex
+  flex-direction: row-reverse
+
+  p
+    padding: 20px
+    margin: 0
+    background-color: #999999
+
+.vtb-item-parent
+  position: relative
+  margin-left: 50px
+  display: flex
+  align-items: center
+
+.vtb-item-players
+  flex-direction: column
+  background-color: #999999
+  margin: 0
+  color: white
+
+  .vtb-player
+    padding: 10px
+
+  .winner
+    background-color: darkgreen
+
+  .defeated
+    background-color: firebrick
+
+  .winner.highlight
+    background-color: darkseagreen
+
+  .defeated.highlight
+    background-color: indianred
+
+.vtb-item-parent:after
+  position: absolute
+  content: ""
+  width: 25px
+  height: 2px
+  left: 0
+  top: 50%
+  background-color: gray
+  transform: translateX(-100%)
+
+.vtb-item-children
+  display: flex
+  flex-direction: column
+  justify-content: center
+
+.vtb-item-child
+  display: flex
+  align-items: flex-start
+  justify-content: flex-end
+  margin-top: 10px
+  margin-bottom: 10px
+  position: relative
+
+  &:before
+    content: ""
+    position: absolute
+    background-color: gray
+    right: 0
+    top: 50%
+    transform: translateX(100%)
+    width: 25px
+    height: 2px
+
+  &:after
+    content: ""
+    position: absolute
+    background-color: gray
+    right: -25px
+    height: calc(50% + 22px)
+    width: 2px
+    top: 50%
+
+  &:last-child:after
+    transform: translateY(-100%)
+
+  &:only-child:after
+    display: none
+
+.vtb-wrapper
+  display: flex
+</style>
